212ba1d832d9eca3c4a1e99135d50c4bbf476f05
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-08 06:25:49 +0200

    built the homepage, bg image optimized with next Image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,17 @@
+import Image from "next/image.js";
 import Link from "next/link.js";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+        quality={100}
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>

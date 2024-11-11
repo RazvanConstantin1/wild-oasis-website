14cac5ee2e384b2461820dcb96848393a966d2cc
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-11 05:57:12 +0200

    set up a filter functionality for cabins list through url params, made a custom filter component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,15 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList.js";
+import Filter from "../_components/Filter.js";
 import Spinner from "../_components/Spinner.js";
-
-export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,9 +23,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );

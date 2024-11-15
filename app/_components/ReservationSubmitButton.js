8e58c21fb563e429817a3f6847adc33cc5af9464
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-15 07:12:36 +0200

    creating a new reservation feature, project kinda feature complete

---
+++ app/_components/ReservationSubmitButton.js
@@ -0,0 +1,16 @@
+import { useFormStatus } from "react-dom";
+
+function ReservationSubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Reserving..." : "Reserve now"}
+    </button>
+  );
+}
+
+export default ReservationSubmitButton;

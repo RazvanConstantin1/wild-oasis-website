8830d85c3a032cac09554c4871d4d75497d89020
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-15 06:17:58 +0200

    implemented new hook use optimistic to instantly delete the reservation from the list

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,4 +1,4 @@
-import ReservationCard from "@/app/_components/ReservationCard";
+import ReservationList from "@/app/_components/ReservationList.js";
 import { auth } from "@/app/_lib/auth.js";
 import { getBookings } from "@/app/_lib/data-service.js";
 
@@ -24,11 +24,7 @@ export default async function Page() {
           </a>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
